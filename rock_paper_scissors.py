e6fd894291e48f5b19e1e4a698df5bd305deeaf8
Devon <defitjo16@gmail.com>
2018-09-03 18:54:49 -0400

    rock, paper, scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,16 +1,17 @@
 from random import randint
+from time import sleep
 
 choices = ["ROCK", "PAPER", "SCISSORS"]
 
 outcome = {
-  "winner": "You Won! ^_^",
-  "loser": "You Lost! -_-",
-  "tie": "It's A Tie! ¯\_(ツ)_/¯"
+  "winner": "You Won! Hip Hip Hooray!!! ^_^",
+  "loser": "You Lost! Humanity is Doomed!!! -_-",
+  "tie": "It's A Tie! Meh, humans and machines will continue to coexist ¯\_(ツ)_/¯"
 }
 
 def winner(user, computer):
   print("Your choice: %s" % user)
-  print("Computer chooses: %s" % computer)
+  print("Skynet chooses: %s" % computer)
   if user == computer:
     print(outcome["tie"])
   elif user == choices[0] and computer == choices[2]:
@@ -19,4 +20,19 @@ def winner(user, computer):
     print(outcome["winner"])
   elif user == choices[2] and computer == choices[1]:
     print(outcome["winner"])
+  else:
+    print(outcome["loser"])
 
+def start_game():
+  print("Your goal is to defeat Skynet in a game of Rock, Paper, or Scissors...")
+  sleep(3)
+  print("Choose wisely, the fate of humanity depends on your decision!")
+  sleep(1)
+  user = input("Choose between Rock, Paper, or Scissors: ")
+  user = user.upper()
+  print("Skynet is choosing...")
+  sleep(2)
+  computer = choices[randint(0, 2)]
+  winner(user, computer)
+
+start_game()
